398260d9832e0fb13bcc978814c0a073af8b7a3b
azer7777 <azertyuio123a77@gmail.com>
2023-04-10 16:01:09 +0200

    header

--- Books-Information.py
+++ Books-Information.py
@@ -1,7 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas
+import lxml
 url = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
 class_name = "table table-striped"
-product_information = soup.find_all("table" , class_=class_name)
+product_information = soup.find_all("table", class_=class_name)
+fd_book = pandas.read_html(url)
+fd_book.remove([0])
+
+    
