1f2ee11699e947686d3e1b6844660a679d219393
azer7777 <azertyuio123a77@gmail.com>
2023-04-10 18:46:00 +0200

    extract table

--- Books-Information.py
+++ Books-Information.py
@@ -1,13 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas
-import lxml
+import csv
 url = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-class_name = "table table-striped"
-product_information = soup.find_all("table", class_=class_name)
-fd_book = pandas.read_html(url)
-fd_book.remove([0])
+class_name = "write_review"
+title = soup.find-all("h1")
+
+book_title = soup.h1.string
+title = ["product_page_url",  "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
+"number_available", "product_description" ,"category", "review_rating", "image_url"]
+product_page_url = soup.find("a", id_=class_name )
+product_page_url
+
+with open('data.csv', 'w') as csv_file:
+    writer = csv.writer(csv_file, delimiter=',')
+    writer.writerow(title)
 
     
