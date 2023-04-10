c9ddedc5d80c5333028174c60106e2e1bf0c7177
azer7777 <azertyuio123a77@gmail.com>
2023-04-10 20:36:32 +0200

    description

--- Books-Information.py
+++ Books-Information.py
@@ -5,12 +5,13 @@ import pandas
 url = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-upc = soup.find_all("td")[0]
-upc.text
+upc = (soup.find_all("td")[0]).text
+
 
 title = ["product_page_url",  "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
 "number_available", "product_description" ,"category", "review_rating", "image_url"]
 product_page_url = url
+upc
 book_title = soup.h1.string
 with open('data.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
