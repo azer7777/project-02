541934f985d114bed750d7d83c4f942a56b45525
azer7777 <azertyuio123a77@gmail.com>
2023-04-11 02:18:44 +0200

    scrap one book

--- Books-Information.py
+++ Books-Information.py
@@ -1,20 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
-import pandas
+
 url = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
 page = requests.get(url)
 soup = BeautifulSoup(page.content, 'html.parser')
-upc = (soup.find_all("td")[0]).text
 
 
 title = ["product_page_url",  "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
 "number_available", "product_description" ,"category", "review_rating", "image_url"]
 product_page_url = url
-upc
+upc = (soup.find_all("td")[0]).text
 book_title = soup.h1.string
+price_including_tax = (soup.find_all("td")[2]).text
+price_excluding_tax = (soup.find_all("td")[3]).text
+number_available = (soup.find_all("td")[5]).text
+product_description = (soup.find_all("p")[3]).text
+category = (soup.find_all("a")[3]).text
+review_rating = soup.find('p', class_='star-rating').get('class')[1] + ' stars'
+image = (soup.find_all("img")[0])
+image_url = "books.toscrape.com"+image.get('src')
+
+description = [product_page_url, upc, book_title, price_including_tax, price_excluding_tax,
+number_available, product_description , category, review_rating, image_url]
+
 with open('data.csv', 'w') as csv_file:
     writer = csv.writer(csv_file, delimiter=',')
-    writer.writerow(title)
+    for t, d in zip(title, description):
+        writer.writerow([t, d])
 
     
