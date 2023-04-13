a4fd291f65cf286aa160f44245d0813d5ef92113
azer7777 <azertyuio123a77@gmail.com>
2023-04-13 15:22:45 +0200

    def

--- Books-Information.py
+++ Books-Information.py
@@ -2,45 +2,52 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-
-url = "http://books.toscrape.com/"
-url_category = "http://books.toscrape.com/catalogue/category/books/travel_2/index.html"
-page = requests.get(url_category)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-url_category_books =[]
-
-for link in soup.select(('.product_pod a')):
-    url_category_books.append(url + "catalogue/" + ((link.get('href').strip('../../../'))))
-
-url_category_books
-
-for url_book in url_category_books:
-    def scrap_one_book(url_book):
-    url_book = "http://books.toscrape.com/catalogue/shakespeares-sonnets_989/index.html"
-    page = requests.get(url_book)
-    soup = BeautifulSoup(page.content, 'html.parser')
+url_main = "http://books.toscrape.com/"
 
 
-    title = ["product_page_url",  "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
-    "number_available", "product_description" ,"category", "review_rating", "image_url"]
+def extract_page(url):
+   page = requests.get(url)
+   soup = BeautifulSoup(page.content, 'html.parser')
+   return soup
 
-    product_page_url = url_book
-    upc = (soup.find_all("td")[0]).text
-    book_title = soup.h1.string
-    price_including_tax = (soup.find_all("td")[2]).text
-    price_excluding_tax = (soup.find_all("td")[3]).text
-    number_available = (soup.find_all("td")[5]).text
-    product_description = (soup.find_all("p")[3]).text
-    category = (soup.find_all("a")[3]).text
-    review_rating = soup.find('p', class_='star-rating').get('class')[1] + ' stars'
-    image = (soup.find_all("img")[0])
-    image_url = "books.toscrape.com"+image.get('src')
 
-    description = [product_page_url, upc, book_title, price_including_tax, price_excluding_tax,
-    number_available, product_description , category, review_rating, image_url]
+def one_category_all_books_urls(url_category):
+    url_category_books =[]
+    soup = extract_page(url_category)
+    for link in soup.select(('.product_pod a')):
+        url_category_books.append(url_main + "catalogue/" + ((link.get('href').strip('../../../'))))
 
-    with open('data.csv', 'w') as csv_file:
+
+    
+def scrap_one_book(url_book):
+        
+        soup = extract_page(url_book)
+
+        title = ["product_page_url",  "universal_ product_code (upc)", "title", "price_including_tax", "price_excluding_tax",
+        "number_available", "product_description" ,"category", "review_rating", "image_url"]
+
+        product_page_url = url_book
+        upc = (soup.find_all("td")[0]).text
+        book_title = soup.h1.string
+        price_including_tax = (soup.find_all("td")[2]).text
+        price_excluding_tax = (soup.find_all("td")[3]).text
+        number_available = (soup.find_all("td")[5]).text
+        product_description = (soup.find_all("p")[3]).text
+        category = (soup.find_all("a")[3]).text
+        review_rating = soup.find('p', class_='star-rating').get('class')[1] + ' stars'
+        image = (soup.find_all("img")[0])
+        image_url = "books.toscrape.com"+image.get('src')
+
+        description = [product_page_url, upc, book_title, price_including_tax, price_excluding_tax,
+        number_available, product_description , category, review_rating, image_url]
+        return (title, description)
+
+
+
+
+
+def transfer_data_by_category('file_name_category'):
+    with open('file_name_category', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',')
         for t, d in zip(title, description):
             writer.writerow([t, d])
